9ed22a332e30f286357d56b30f696c06ba357466
SarthakAdhikari <adhikari.sarthak@gmail.com>
2024-07-22 17:30:23 -0500

    add multi character tokens

--- scanner.py
+++ scanner.py
@@ -1,5 +1,5 @@
 from typing import List
-from token import Token
+from lox_token import Token
 from token_type import TokenType
 from lox import Lox
 
@@ -20,7 +20,7 @@ class Scanner:
         self.tokens.append(Token(TokenType.EOF, "", None, self.line))
         return list()
     
-    def scan_token(self):
+    def scan_token(self) -> None:
         c = self.advance()
         if c == '(':
             self.add_token(TokenType.LEFT_PAREN)
@@ -42,23 +42,51 @@ class Scanner:
             self.add_token(TokenType.SEMICOLON)
         elif c == '*':
             self.add_token(TokenType.STAR)
+        elif c == '!':
+            self.add_token(
+                TokenType.BANG_EQUAL if self.match('=') 
+                else TokenType.BANG
+            )
+        elif c == '<':
+            self.add_token(
+                TokenType.LESS_EQUAL if self.match('=') 
+                else TokenType.LESS
+            )
+        elif c == '>':
+            self.add_token(
+                TokenType.GREATER_EQUAL if self.match('=') 
+                else TokenType.GREATER
+            )
+        elif c == '=':
+            self.add_token(
+                TokenType.EQUAL_EQUAL if self.match('=') 
+                else TokenType.EQUAL
+            )
         else:
             Lox.error(self.line, "Unexpected character.")
             
-
-
-    def add_token(self,token_type: TokenType, literal: object = None):
+    def add_token(self,token_type: TokenType, literal: object = None) -> None:
         text = self.source[self.start:self.current]
         token = Token(token_type, text, literal, self.line)
         self.tokens.append(token_type)
         print(self.tokens)
 
-    def advance(self):
+
+    def advance(self) -> str:
         assert self.current < len(self.source), "Reached end of source."
         current_char = self.source[self.current]
         self.current += 1
         return current_char
+
+    def match(self, excepted: str) -> bool:
+        if self.is_at_end():
+            return False
+        if self.source[self.current] != excepted:
+            return False
         
+        self.current += 1
+        return True
+
     def is_at_end(self) -> bool:
         return self.current >= len(self.source)
         
